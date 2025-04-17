b00469a6b77521e854b29c701458e1a266803654
alessiaianes <ianesalessia2@gmail.com>
2025-04-17 20:12:45 +0000

    Update pivot table to use 'Test PPL' and add new LSTM performance result CSV files

--- 256181_Alessia_Ianes/LM/part_B/main.py
+++ 256181_Alessia_Ianes/LM/part_B/main.py
@@ -158,7 +158,7 @@ if __name__ == "__main__":
 
 
     pivot_table = results_df.pivot_table(
-        values='Final PPL',
+        values='Test PPL',
         index='Batch Size',  # Rows: Batch Size
         columns='Learning Rate'  # Columns: Learning Rate
     )
